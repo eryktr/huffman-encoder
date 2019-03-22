f8d77291a8137d3b70edbef9a8ad7bed2adec624
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2019-03-22 17:15:01 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace huffman_encoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
